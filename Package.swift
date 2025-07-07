7835dc37bc9423e1ce11ed74412db60594b1a2f6
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-07-07 23:34:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CameraModule",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
